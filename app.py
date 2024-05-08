1cadf48b3438013e4f076c1f2ef568f317743288
MARCELO CLARO LARANJEIRA <58664974+MarceloClaro@users.noreply.github.com>
2024-05-07 22:02:13 -0300

    Update app.py

--- app.py
+++ app.py
@@ -38,8 +38,15 @@ def main():
     primary_prompt = st.sidebar.text_input("Prompt do sistema principal", "Como posso ajudar você hoje?")
     secondary_prompt = st.sidebar.text_input("Prompt do sistema secundário", "Há algo mais em que posso ajudar?")
     model_choice = st.sidebar.selectbox("Escolha um modelo", ["llama3-70b-8192", "llama3-8b-8192", "mixtral-8x7b-32768", "gemma-7b-it"])
-    conversational_memory_length = st.sidebar.slider('Tamanho da memória conversacional', 1, 10, value=5)
-
+    conversational_memory_length = st.sidebar.slider('Tamanho da memória conversacional', 1, 50, value=5)
+    st.image("eu.ico", width=100)
+    st.write("""
+    Projeto Geomaker + IA 
+    - Professor: Marcelo Claro.
+    Contatos: marceloclaro@gmail.com
+    Whatsapp: (88)981587145
+    Instagram: https://www.instagram.com/marceloclaro.geomaker/
+    """)
     memory = ConversationBufferWindowMemory(k=conversational_memory_length, memory_key="chat_history", return_messages=True)
     if 'chat_history' not in st.session_state:
         st.session_state.chat_history = []
